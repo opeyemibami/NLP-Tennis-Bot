ea9f2ba40d617ce41936cd1999efc9bcc81057d1
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-06-29 17:15:38 +0100

    local hosting test passed

---
+++ app.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import preprocessor as p
+from tensorflow.keras.models import load_model
+import joblib
+from pathlib import Path 
+from PIL import Image
+import streamlit as st
+
+#paths
+img_path = Path.joinpath(Path.cwd(),'images')
+artifacts_path = Path.joinpath(Path.cwd(),'model_artifacts')
+datasets_path = Path.joinpath(Path.cwd(),'dataset')
+
+#load images 
+center = Image.open(Path.joinpath(img_path,'center.jpg'))
+federer_image = Image.open(Path.joinpath(img_path,'federer.jpg'))
+nadal =Image.open(Path.joinpath(img_path,'Nadal.jpg'))
+
+#load artifacts 
+model = load_model(Path.joinpath(artifacts_path,'model-v1.h5'))
+tokenizer_t = joblib.load(Path.joinpath(artifacts_path,'tokenizer_t.pkl'))
+vocab = joblib.load(Path.joinpath(artifacts_path,'vocab.pkl'))
+
+df2 = pd.read_csv(Path.joinpath(datasets_path,'response.csv'))
+
+def get_pred(model,encoded_input):
+    pred = np.argmax(model.predict(encoded_input))
+    return pred
+
+def bot_precausion(df_input,pred):
+    words = df_input.questions[0].split()
+    if len([w for w in words if w in vocab])==0 :
+        pred = 1
+    return pred
+
+def get_response(df2,pred):
+    upper_bound = df2.groupby('labels').get_group(pred).shape[0]
+    r = np.random.randint(0,upper_bound)
+    responses = list(df2.groupby('labels').get_group(pred).response)
+    return responses[r]
+
+def bot_response(response,):
+    return response
+
+
+def botResponse(user_input,is_startup=True):
+    df_input = user_input
+    
+    df_input = p.remove_stop_words_for_input(p.tokenizer,df_input,'questions')
+    encoded_input = p.encode_input_text(tokenizer_t,df_input,'questions')
+
+    pred = get_pred(model,encoded_input)
+    pred = bot_precausion(df_input,pred)
+
+    response = get_response(df2,pred)
+    response = bot_response(response)
+    
+    if is_startup:
+        response = "Hi, I'm happy to have you here \nI have a lot to discuss about tennis"
+        is_startup = False
+        return response
+
+    else:
+        return  response
+
+def get_text():
+    input_text = st.text_input("You: ","type here")
+    df_input = pd.DataFrame([input_text],columns=['questions'])
+    return df_input 
+
+
+
+st.sidebar.title("BoTennis")
+st.title("""
+BoTennis  
+BoTennis is a NLP bot trainned basic tennis corpus using  CNN achitecture
+""")
+
+st.image(center,width=700)
+st.sidebar.image(federer_image)
+st.sidebar.image(nadal,width=350)
+
+user_input = get_text()
+response = botResponse(user_input)
+st.text_area("Bot:", value=response, height=200, max_chars=None, key=None)
---
+++ preprocessor.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+import string
+import re
+import json
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+
+def tokenizer(entry):
+    tokens = entry.split()
+    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
+    tokens = [re_punc.sub('', w) for w in tokens]
+    tokens = [word for word in tokens if word.isalpha()]
+    tokens = [lemmatizer.lemmatize(w.lower()) for w in tokens]
+#     stop_words = set(stopwords.words('english'))
+#     tokens = [w for w in tokens if not w in stop_words]
+    tokens = [word.lower() for word in tokens if len(word) > 1]
+    return tokens
+
+
+def remove_stop_words_for_input(tokenizer,df,feature):
+    doc_without_stopwords = []
+    entry = df[feature][0]
+    tokens = tokenizer(entry)
+    doc_without_stopwords.append(' '.join(tokens))
+    df[feature] = doc_without_stopwords
+    return df
+
+def encode_input_text(tokenizer_t,df,feature):
+    t = tokenizer_t
+    entry = entry = [df[feature][0]]
+    encoded = t.texts_to_sequences(entry)
+    padded = pad_sequences(encoded, maxlen=16, padding='post')
+    return padded
